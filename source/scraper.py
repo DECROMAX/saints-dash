1b5643e0be76ca59a4c2a997c1222096fbe17a3b
DECROMAX <rjlynch@btinternet.com>
2026-01-28 21:17:19 +0000

    Enhance data preprocessing: set pandas display options, drop unused columns, and add DataFrame printout., added helper columns `home_away_flag`, `points`

--- source/scraper.py
+++ source/scraper.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+# pandas display options
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 
 COL_LABELS = {
     "Div": "division",
@@ -26,3 +30,5 @@ df = pd.read_csv('https://www.football-data.co.uk/mmz4281/2526/E1.csv', usecols=
 df = df.rename(columns=COL_LABELS)
 
 df['match_date_time'] = pd.to_datetime((df['match_date'] + ' ' + df['kickoff_time']), format='mixed', dayfirst=True)
+df = df.drop(columns=['match_date', 'kickoff_time'])
+print(df)
